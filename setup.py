6142b0ec98004d0a1b4dac41e4e63a75a1576ecb
Xun Gong <gongxun@sjtu.edu.cn>
2025-06-18 11:43:33 +0800

    update bump version=0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytorchocr',
-    version='0.1.0',
+    version='0.2.0',
     description='OCR inference package based on PyTorch',
     author='Your Name',
     author_email='your.email@example.com',
